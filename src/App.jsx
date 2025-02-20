333940d3c131abe2dfcc7a244ab18b0176479c64
Track404 <111713000+Track404@users.noreply.github.com>
2025-02-20 14:08:38 +0100

    add tailwind utilites

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import LoginPage from './pages/LoginPage';
-
+import SignUpPage from './pages/SignUpPage';
 function App() {
   return (
     <>
-      <LoginPage />
+      <SignUpPage />
     </>
   );
 }
