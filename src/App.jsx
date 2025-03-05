084bfc7fee31f6c3fff0a589078fd69e4d8a7090
Track404 <111713000+Track404@users.noreply.github.com>
2025-03-05 14:33:43 +0100

    creation of the react router dom routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,50 @@
+import LoginPage from './pages/LoginPage';
+import MainPage from './pages/MainPage';
+import NewDisccusion from './pages/NewDiscussion';
+import SignUpPage from './pages/SignUpPage';
 import UserDisscussion from './pages/UserDiscussion';
+import UserPage from './pages/UserPage';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Navigate to="/login" replace />,
+  },
+  {
+    path: '/login',
+    element: <LoginPage />,
+  },
+  {
+    path: '/register',
+    element: <SignUpPage />,
+  },
+  {
+    path: '/Interface',
+    element: <MainPage />,
+  },
+  {
+    path: '/newDiscussion',
+    element: <NewDisccusion />,
+  },
+  {
+    path: '/userDiscussion/:id',
+    element: <UserDisscussion />,
+  },
+  {
+    path: '/userPage/:id',
+    element: <UserPage />,
+  },
+]);
 function App() {
   return (
     <>
-      <UserDisscussion />
+      <RouterProvider router={router} />
     </>
   );
 }
