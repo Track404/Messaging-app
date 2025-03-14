dc0f202c63c0bd7237a25b29abd700bd689f40f4
Track404 <111713000+Track404@users.noreply.github.com>
2025-03-14 17:03:58 +0100

    creation of french NewDiscussion page

--- src/pages-fr/NewDiscussion-fr.jsx
+++ src/pages-fr/NewDiscussion-fr.jsx
@@ -93,18 +93,18 @@ function NewDisccusionFr() {
           <div className="h-screen flex flex-col shadow-2xl md:md:min-w-[40vw] xl:min-w-[30vw] relative">
             <div className="shadow-md ">
               <h2 className="text-white text-4xl text-left font-semibold p-4 w-full bg-[url(./assets/hive-background.svg)] bg-cover">
-                NEW DISCUSSION
+                Discussion
               </h2>
             </div>
             <button
               onClick={() => {
-                navigate('/newGroup');
+                navigate('/nouveauGroupe');
               }}
               className="border-b-2 text-2xl font-bold p-4"
             >
               <div className="flex items-center gap-2 hover:scale-105 active:scale-100 ">
                 <Users className="w-11 h-11 text-amber-400" />
-                <h2>New Group</h2>
+                <h2>Nouveau Groupe</h2>
               </div>
             </button>
             <div className="h-full  overflow-auto">
@@ -125,7 +125,7 @@ function NewDisccusionFr() {
             <div className="border-t-2 text-2xl font-bold p-4 md:hidden">
               <div className="flex items-center gap-2 ">
                 <Send className="w-11 h-11 text-amber-400" />
-                To: {userSendId?.name || 'Select a user'}
+                À: {userSendId?.name || 'Sélectionner un utilisateur'}
               </div>
             </div>
             <div className="flex justify-around items-center border-t-2 h-30 bg-neutral-50 ">
@@ -139,7 +139,7 @@ function NewDisccusionFr() {
                       strokeWidth="1.25"
                       className="w-11 h-11 text-amber-400"
                     />
-                    <p className="font-medium text-lg">Create Chat</p>
+                    <p className="font-medium text-lg">Créer</p>
                   </div>
                 </button>
               </form>
@@ -155,7 +155,7 @@ function NewDisccusionFr() {
                     strokeWidth="1.25"
                     className="w-11 h-11 text-amber-400"
                   />
-                  <p className="font-medium text-lg">Go Back</p>
+                  <p className="font-medium text-lg">Retour</p>
                 </div>
               </button>
             </div>
@@ -163,7 +163,11 @@ function NewDisccusionFr() {
           <div className="hidden w-0 md:flex md:flex-col md:w-full md:h-screen">
             <div className="flex flex-col w-full h-screen md:bg-[url(./assets/messageBackgournd.svg)] md:bg-contain">
               <div className="bg-white w-full shadow-3xl border-b-1">
-                <ChatName name={`To: ${userSendId?.name || 'Select a user'}`} />
+                <ChatName
+                  name={`À: ${
+                    userSendId?.name || 'Sélectionner un utilisateur'
+                  }`}
+                />
               </div>
 
               <div className="flex flex-col-reverse overflow-y-auto w-full flex-grow"></div>
